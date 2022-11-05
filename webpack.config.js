43e784eb54110b28dddc151d6798c8faf3f89fc7
gyan <gyan9427@gmail.com>
2022-11-05 21:38:27 +0530

    create index.html file and add plugins to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: "development",
@@ -6,5 +8,12 @@ module.exports = {
     output: {
         filename: './main.bundle.js',
         path: path.resolve(__dirname,'dist')
-    }
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 }
